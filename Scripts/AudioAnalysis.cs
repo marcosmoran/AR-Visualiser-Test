4a4f8cffa9f3b6e1f5b002d3d03d1697ef509ff6
Marcos Moran <marcosmoran62@hotmail.com>
2020-01-20 10:54:10 +0000

    initial commit

---
+++ Scripts/AudioAnalysis.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (AudioSource))]
+public class AudioAnalysis : MonoBehaviour
+{
+    AudioSource _audioSource;
+    public static float[] _samples = new float[512];
+    public static float[] _freqBands = new float[8];
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetSpectrumAudioSource();
+        MakeFreqBands();
+    }
+
+    void GetSpectrumAudioSource()
+    {
+        _audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
+    }
+    void MakeFreqBands()
+    {
+        int count = 0;
+
+        for(int i = 0; i < 8; i++)
+        {
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+
+            if (i == 7)
+            {
+                sampleCount += 2;
+            }
+            for (int j = 0; j < sampleCount; j++)
+            {
+                average += _samples[count] * (count + 1);
+                count++;
+            }
+            average /= count;
+            _freqBands[i] = average * 10;
+        }
+    }
+}
---
+++ Scripts/ModelReactivity.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelReactivity : MonoBehaviour
+{
+    public int _band;
+    public float _startScale, _scaleMultiplier;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = new Vector3(transform.localScale.x, (AudioAnalysis._freqBands[_band] * _scaleMultiplier) + _startScale, transform.localScale.z);
+    }
+}
