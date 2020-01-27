9512367d3365acc1ff29bdea8e570243dc5072e3
Marcos Moran <marcosmoran62@hotmail.com>
2020-01-27 21:34:26 +0000

    testing new particle reactivity scene

---
+++ Scripts/AtomicAttraction.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtomicAttraction : MonoBehaviour
+{
+    public GameObject _atom, _attractor;
+    public Gradient _gradient;
+    public int[] _attractPoints;
+    public Vector3 _spacingDirection;
+    [Range(0,20)]
+    public float _spacingBetweenAttractPoints;
+    [Range(0, 20)]
+    public float _scaleAttractPoints;
+
+
+    //visualises the attraction points on scene window and allows for ease of manipulating them
+    private void OnDrawGizmos()
+    {
+        for( int i = 0; i < _attractPoints.Length; i++)
+        {
+            //colour the instanced objects
+            float evaluateStep = 1.0f / _attractPoints.Length;
+            Color color = _gradient.Evaluate(evaluateStep * i);
+            Gizmos.color = color;
+            Vector3 pos = new Vector3(transform.position.x + (_spacingBetweenAttractPoints * i * _spacingDirection.x),
+                                      transform.position.y + (_spacingBetweenAttractPoints * i * _spacingDirection.y),
+                                      transform.position.z + (_spacingBetweenAttractPoints * i * _spacingDirection.z));
+            Gizmos.DrawSphere(pos, _scaleAttractPoints);
+
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Scripts/AttractTo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttractTo : MonoBehaviour
+{
+    Rigidbody _rigidbody;
+    public Transform _attractedTo;
+    public float _strengthOfAttraction, _maxMagnitude;
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody> ();
+
+    }
+
+   
+    void Update()
+    {
+        if (_attractedTo != null)
+        {
+            //direction of applied force
+            Vector3 direction = _attractedTo.position - transform.position;
+            _rigidbody.AddForce(_strengthOfAttraction * direction);
+           
+            // limit magnitude
+            if(_rigidbody.velocity.magnitude > _maxMagnitude)
+            {
+                _rigidbody.velocity = _rigidbody.velocity.normalized * _maxMagnitude;
+            }
+        
+        
+        }
+    }
+}
